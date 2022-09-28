2c36b913f032aa5cef2cb0d7053a83cf599cbe1e
matekelemen <kelemen.mate.12@gmail.com>
2022-09-28 11:48:40 +0200

    wip: add autocompletion

--- install.py
+++ install.py
@@ -8,59 +8,60 @@ import shutil
 import os
 
 # Get source directories
-thisScript = pathlib.Path(__file__).absolute()
-sourceDir  = thisScript.parent
-scriptDir  = sourceDir / "scripts"
-configDir  = sourceDir / "config"
+thisScript      = pathlib.Path(__file__).absolute()
+sourceDir       = thisScript.parent
+scriptDir       = sourceDir / "scripts"
+autocompleteDir = scriptDir / ".autocomplete"
+configDir       = sourceDir / "config"
 
 # CLI
 parser = argparse.ArgumentParser(description="Install shell scripts and config files")
-parser.add_argument(
-    "installPrefix",
-    metavar="install_prefix",
-    type=str,
-    nargs='?',
-    default=str(pathlib.Path.home() / ".local" / "bin"),
-    help="destination directory for executable scripts"
-)
+parser.add_argument("installPrefix",
+                    metavar = "install_prefix",
+                    type = str,
+                    nargs = '?',
+                    default = str(pathlib.Path.home() / ".local" / "bin"),
+                    help = "destination directory for executable scripts")
 
-parser.add_argument(
-    "-c",
-    "--config-prefix",
-    dest="configPrefix",
-    type=str,
-    default=str(pathlib.Path.home()),
-    help="destination directory for configuration files ($HOME by default)"
-)
+parser.add_argument("-a",
+                    "--autocomplete-prefix",
+                    dest = "autocompletePrefix",
+                    type = str,
+                    default = str(pathlib.Path.home() / ".local" / "bin" / ".autocomplete"),
+                    help = "destination directory for autocomplete files ($HOME/.local/bin/.autocomplete by default)")
 
-parser.add_argument(
-    "-y",
-    "--overwrite",
-    action="store_const",
-    default=False,
-    const=True,
-    help="overwrite existing files during installation"
-)
+parser.add_argument("-c",
+                    "--config-prefix",
+                    dest = "configPrefix",
+                    type = str,
+                    default = str(pathlib.Path.home()),
+                    help = "destination directory for configuration files ($HOME by default)")
 
-parser.add_argument(
-    "-s",
-    "--install-symlinks",
-    dest = "installSymlinks",
-    action = "store_const",
-    default = False,
-    const = True,
-    help = "Create symlinks instead of copying files at install"
-)
+parser.add_argument("-y",
+                    "--overwrite",
+                    action = "store_const",
+                    default = False,
+                    const = True,
+                    help = "overwrite existing files during installation")
+
+parser.add_argument("-s",
+                    "--install-symlinks",
+                    dest = "installSymlinks",
+                    action = "store_const",
+                    default = False,
+                    const = True,
+                    help = "Create symlinks instead of copying files at install")
 
 # Parse command line arguments
 arguments = parser.parse_args(sys.argv[1:])
 
 # Make sure destination directories exist
 scriptInstallDir = pathlib.Path(arguments.installPrefix).absolute()
+autocompleteInstallDir = pathlib.Path(arguments.autocompletePrefix).absolute()
 configInstallDir = pathlib.Path(arguments.configPrefix).absolute()
 
-scriptInstallDir.mkdir(parents=True, exist_ok=True)
-configInstallDir.mkdir(parents=True, exist_ok=True)
+for directory in (scriptInstallDir, autocompleteInstallDir, configInstallDir):
+    directory.mkdir(parents = True, exist_ok = True)
 
 installFunctor = lambda source, destination: os.symlink(str(source), str(destination)) if arguments.installSymlinks else shutil.copy(str(source), str(destination))
 
@@ -81,12 +82,13 @@ def copyFile(source: pathlib.Path, destination: pathlib.Path):
         destination.parent.mkdir(exist_ok=True, parents=True)
         installFunctor(item, destination)
 
-# Copy scripts
-for item in scriptDir.glob("*"):
-    if item.is_file():
-        name = str(item.name)
-        destination = scriptInstallDir / name
-        copyFile(item, destination)
+# Copy scripts and autocompletes
+for sourcePrefix, destinationPrefix in ((scriptDir, scriptInstallDir), (autocompleteDir, autocompleteInstallDir)):
+    for item in sourcePrefix.glob("*"):
+        if item.is_file():
+            name = str(item.name)
+            destination = destinationPrefix / name
+            copyFile(item, destination)
 
 # Copy config files
 configProperties = {
